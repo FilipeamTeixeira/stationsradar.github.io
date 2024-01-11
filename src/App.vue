9e425e158f3d49b3f81865a8b293e29fb8bd4cab
FilipeamTeixeira <Filipe.MarquesTeixeira@Ugent.be>
2024-01-11 18:38:30 +0100

    legacy updated

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,60 @@
 <template>
-  <v-app class="grey lighten-4 background: #FF44FF; ">
-    <Navbar/>
-    <v-content>
-     <vue-page-transition name="overlay-right">
-     <router-view></router-view>
-     </vue-page-transition>
-    </v-content>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
   </v-app>
 </template>
 
 <script>
-
-import Navbar from './components/Navbar'
+import HelloWorld from './components/HelloWorld';
 
 export default {
-  components: { Navbar },
   name: 'App',
-data() {
-    return {
-    }
-   }
-  }
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
-<style>
-table.v-table tbody td {
-    font-size: 18px !important;
-}
-   .overlay-left,  .overlay-right,  .overlay-top,  .overlay-bottom   {
-    background: #EDCB64 !important;
-}
-</style>
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,4 @@
-// vue.config.js
+
 module.exports = {
-    lintOnSave: false,
-    transpileDependencies: [
-      'vuetify' // Add any other dependencies that need to be transpiled
-    ],
-    // Other configurations...
+    transpileDependencies: ['vuetify'],
   };
