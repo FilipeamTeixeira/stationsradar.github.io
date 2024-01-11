e9ca5955bf121c90715f2adeaf5844962ac7bd8b
FilipeamTeixeira <Filipe.MarquesTeixeira@Ugent.be>
2024-01-11 17:02:03 +0100

    test

---
+++ vue.config.js
@@ -0,0 +1,7 @@
+// vue.config.js
+module.exports = {
+    transpileDependencies: [
+      'vuetify' // Add any other dependencies that need to be transpiled
+    ],
+    // Other configurations...
+  };
